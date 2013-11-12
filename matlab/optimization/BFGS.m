b6af2e8bfa5e18eba466302371f4e6736eb9d793
CyberZHG <CyberZHG@gmail.com>
2013-11-12 16:42:07 +0800

    Optimization => BFGS
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- matlab/optimization/BFGS.m
+++ matlab/optimization/BFGS.m
@@ -1,39 +1,24 @@
-syms x1  x2 lambda;
-f = x1 ^ 2 + x1 * x2 + x2 * x2;
-g = gradient(f);
-x0 = [3; 2];
-Hk = [1, 0; 0, 1];
-disp(['Iteration 1']);
-gk = subs(g, [x1, x2], x0);
-sk = -gk;
-fl = subs(f, [x1, x2], x0 + sk .* lambda);
-fld = diff(fl);
-step = solve(fld);
-xk = x0 + step .* sk;
-detxk = xk - x0;
-disp(['x = ' char(xk')]);
-disp(['f = ' char(subs(f, [x1, x2], xk))]);
-disp(' ');
-for i = 2 : 100
-    gkk = subs(g, [x1, x2], xk);
-    if sum(gkk .^ 2) < 1e-6
+function [x_min, f_min] = BFGS(func, param, x0)
+grad = gradient(func);
+g0 = subs(grad, param, x0);
+s0 = - g0;
+H0 = eye(size(x0, 1));
+x1 = LinearSearch(func, param, x0, s0);
+detx = x1 - x0;
+for i = 1 : 100
+    g1 = subs(grad, param, x1);
+    detg = g1 - g0;
+    if norm(g1) < 1e-6
         break
     end
-    disp(['Iteration ' num2str(i)]);
-    detgk = gkk - gk;
-    muk = 1 + detgk' * Hk * detgk / (detxk' * detgk);
-    Hkk = Hk + (muk * detxk * detxk' - Hk * detgk * detxk' - detxk * detgk' * Hk) / (detxk' * detgk);
-    skk = - Hkk * gkk;
-    fl = simplify(subs(f, [x1, x2], xk + skk .* lambda));
-    fld = diff(fl);
-    step = solve(fld);
-    xkk = xk + step .* skk;
-    
-    detxk = xkk - xk;
-    xk = xkk;
-    gk = gkk;
-    Hk = Hkk;
-    disp(['x = ' char(xk')]);
-    disp(['f = ' char(subs(f, [x1, x2], xk))]);
-    disp(' ');
+    mu = 1 + (detg' * H0 * detg) / (detx' * detg);
+    H1 = H0 + (mu * detx * detx' - H0 * detg * detx' - detx * detg' * H0) / (detx' * detg);
+    s1 = - H1 * g1;
+    x0 = x1;
+    x1 = LinearSearch(func, param, x1, s1);
+    detx = x1 - x0;
+    g0 = g1;
+    H0 = H1;
 end
+x_min = double(x1);
+f_min = double(subs(func, param, x_min));
