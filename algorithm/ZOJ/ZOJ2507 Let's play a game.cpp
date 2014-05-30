1e9e7e6f9252e15d1f12d5b576ab0e1fc93e3c89
CyberZHG <CyberZHG@gmail.com>
2014-05-31 00:54:41 +0800

    Anti-Nim
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

---
+++ algorithm/ZOJ/ZOJ2507 Let's play a game.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        int a, cnt = 0, sg = 0;
+        while (n--)
+        {
+            scanf("%d", &a);
+            cnt += a > 1;
+            sg ^= a;
+        }
+        if (sg > 0 == cnt > 0)
+        {
+            puts("1");
+        }
+        else
+        {
+            puts("2");
+        }
+    }
+    return 0;
+}
