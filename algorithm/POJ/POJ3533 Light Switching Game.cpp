82a3391a3202d0617dc166ca768f16dc4ad8023e
cyberzhg <cyberzhg@gmail.com>
2014-05-31 18:50:14 +0800

    POJ3533
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

---
+++ algorithm/POJ/POJ3533 Light Switching Game.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int nim[2][2] = {0, 0, 0, 1};
+
+int nimMultiLowest(int x)
+{
+    for (int i = 0; ; ++i)
+    {
+        if ((1 << (1 << i)) > x)
+        {
+            return 1 << (1 << (i - 1));
+        }
+    }
+}
+
+int nimMultiPower(int x, int y)
+{
+    if (x < 2 && y < 2)
+    {
+        return nim[x][y];
+    }
+    int m = nimMultiLowest(x);
+    int p = x / m;
+    int s = y / m;
+    int t = y - s * m;
+    int d1 = nimMultiPower(p, s);
+    int d2 = nimMultiPower(p, t);
+    return (m * (d1 ^ d2)) ^ nimMultiPower(m >> 1, d1);
+}
+
+int nimMulti(int x, int y)
+{
+    if (x < 2 && y < 2)
+    {
+        return nim[x][y];
+    }
+    if (x < y)
+    {
+        return nimMulti(y, x);
+    }
+    int m = nimMultiLowest(x);
+    int p = x / m;
+    int q = x - p * m;
+    int s = y / m;
+    int t = y - s * m;
+    int c1 = nimMulti(p, s);
+    int c2 = nimMulti(p, t) ^ nimMulti(q, s);
+    int c3 = nimMulti(q, t);
+    return (m * (c1 ^ c2)) ^ c3 ^ nimMultiPower(m >> 1, c1);
+}
+
+int main()
+{
+    int n, x, y, z;
+    while (~scanf("%d", &n))
+    {
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            scanf("%d%d%d", &x, &y, &z);
+            ans ^= nimMulti(nimMulti(x, y), z);
+        }
+        puts(ans ? "No" : "Yes");
+    }
+    return 0;
+}
