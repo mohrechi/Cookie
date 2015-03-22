2a16d70be2447014d3aa8ad223f4f8cf591101a6
cyberzhg <cyberzhg@gmail.com>
2015-03-22 15:13:49 +0800

    Update summary.py.
    Languages
    ====================================================
    pas                 File:    1          Line:     28
    c                   File:  239          Line:   6900
    cpp                 File: 1695          Line: 132385
    php                 File:    2          Line:     28
    scala               File:   10          Line:    175
    go                  File:    2          Line:     52
    py                  File:   56          Line:    932
    sql                 File:   10          Line:     58
    java                File:   54          Line:   2715
    ====================================================
    
    Online Judges
    ====================================================
    POJ                 File:  673          Line:  51535
    HDU                 File:  305          Line:  25868
    DOJ 2009            File:  175          Line:  13709
    DOJ 2013            File:  194          Line:  11886
    Codeforces          File:  210          Line:  10625
    USACO               File:   97          Line:   8384
    LeetCode            File:  180          Line:   5421
    SPOJ                File:   30          Line:   4702
    DOJ 2011            File:   78          Line:   2925
    URAL                File:   40          Line:   2247
    UVa                 File:   15          Line:   1590
    TopCoder            File:   32          Line:   1574
    ZOJ                 File:   23          Line:   1395
    HackerRank          File:    9          Line:    715
    UESTC               File:    5          Line:    292
    BZOJ                File:    1          Line:    275
    SCU                 File:    2          Line:    130
    ====================================================
    Total               File: 2069          Line: 143273
    
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- summary.py
+++ summary.py
@@ -1,20 +1,45 @@
 import os
 import codecs
 
+lang_file = {}
+lang_line = {}
+
 def count_num(path):
+    global lang_file
+    global lang_line
     file_num = 0
     line_num = 0
     for dir_name in os.listdir(path):
         file_path = os.path.join(path, dir_name)
         if os.path.isdir(file_path):
             dir_file_num, dir_line_num = count_num(file_path)
-            file_num = file_num + dir_file_num
-            line_num = line_num + dir_line_num
+            file_num += dir_file_num
+            line_num += dir_line_num
         else:
             with open(file_path) as file_handle:
-                file_num = file_num + 1
-                line_num = line_num + len(file_handle.readlines())
+                current_line_num = len(file_handle.readlines())
+                file_num += 1
+                line_num += current_line_num
+                ext = file_path.split('.')[-1]
+                if ext in lang_file.keys():
+                    lang_file[ext] += 1
+                    lang_line[ext] += current_line_num
+                else:
+                    lang_file[ext] = 1
+                    lang_line[ext] = current_line_num
     return file_num, line_num
+
+
+def leading_space(str, num):
+    while len(str) < num:
+        str = ' ' + str
+    return str
+
+
+def tailing_space(str, num):
+    while len(str) < num:
+        str += ' '
+    return str
 
 names = []
 file_nums = []
@@ -32,9 +57,23 @@ for i in range(len(names)):
             names[i], names[j] = names[j], names[i]
             file_nums[i], file_nums[j] = file_nums[j], file_nums[i]
             line_nums[i], line_nums[j] = line_nums[j], line_nums[i]
-names.append('Total')
-file_nums.append(sum(file_nums))
-line_nums.append(sum(line_nums))
+
+print('Languages')
+print('=' * 52)
+for ext in lang_file.keys():
+    print(tailing_space(ext, 20) + 
+          'File:' + leading_space(str(lang_file[ext]), 5) + 
+          '\t\tLine:' + leading_space(str(lang_line[ext]), 7))
+print('=' * 52)
+print('')
+
+print('Online Judges')
+print('=' * 52)
 for i in range(len(names)):
-    space = ''.join([' ' for j in range(24 - len(names[i]))])
-    print(names[i] + space + 'File: ' + str(file_nums[i]) + '\t\tLine: ' + str(line_nums[i]))
+    print(tailing_space(names[i], 20) + 
+          'File:' + leading_space(str(file_nums[i]), 5) + 
+          '\t\tLine:' + leading_space(str(line_nums[i]), 7))
+print('=' * 52)
+print(tailing_space('Total', 20) + 
+      'File:' + leading_space(str(sum(file_nums)), 5) + 
+      '\t\tLine:' + leading_space(str(sum(line_nums)), 7))
