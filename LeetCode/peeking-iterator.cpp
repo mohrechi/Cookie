202c60f6bb0c637e00b46e804946ecb168e1a962
cyberzhg <cyberzhg@gmail.com>
2015-09-21 12:16:02 +0800

    LeetCode
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

---
+++ LeetCode/peeking-iterator.cpp
@@ -0,0 +1,29 @@
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	int next();
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	}
+
+	int peek() {
+        return Iterator(*this).next();
+	}
+
+	int next() {
+	    return Iterator::next();
+	}
+
+	bool hasNext() const {
+	    return Iterator::hasNext();
+	}
+};
