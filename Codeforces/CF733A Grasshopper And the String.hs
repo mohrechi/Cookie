4b93600b870b37e46026bbfa4ce8d9f72d316df2
CyberZHG <CyberZHG@gmail.com>
2016-11-06 13:26:50 +0800

    Codeforces
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- Codeforces/CF733A Grasshopper And the String.hs
+++ Codeforces/CF733A Grasshopper And the String.hs
@@ -6,5 +6,5 @@ vowels = "AEIOUY"
 
 solve :: Int -> String -> Int
 solve last []    = last
-solve last (c:s) | elem c vowels = max last (solve 1 s)
+solve last (c:s) | c `elem` vowels = max last (solve 1 s)
                  | otherwise = solve (last + 1) s
