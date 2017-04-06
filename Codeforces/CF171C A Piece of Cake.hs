31ef34ab0696dcc118f4fe747dd12ba0966afc6a
CyberZHG <CyberZHG@gmail.com>
2017-04-06 10:04:34 +0800

    Codeforces
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- Codeforces/CF171C A Piece of Cake.hs
+++ Codeforces/CF171C A Piece of Cake.hs
@@ -2,4 +2,4 @@ main :: IO ()
 main = do
     s <- getLine
     let a = map read $ tail $ words s :: [Integer]
-    print $ sum $ map (\x -> fst x * snd x) $ zip a [1..]
+    print $ sum $ map (uncurry (*)) $ zip a [1..]
