7f386fc96442ca78ffaf0ca4089219e6b5095519
CyberZHG <CyberZHG@gmail.com>
2017-04-11 09:50:46 +0800

    Codeforces
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- Codeforces/CF409C Magnum Opus.hs
+++ Codeforces/CF409C Magnum Opus.hs
@@ -1,4 +1,4 @@
 import Data.List
 
 main :: IO ()
-main = print . minimum . zipWith (\x y -> y `div` x) [1, 1, 2, 7, 4] . map read . words =<< getLine
+main = print . minimum . zipWith (flip div) [1, 1, 2, 7, 4] . map read . words =<< getLine
