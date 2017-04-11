0a05c532884c41b8518353aac0700e9bffb97f34
CyberZHG <CyberZHG@gmail.com>
2017-04-11 10:44:21 +0800

    Codeforces
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- Codeforces/CF409G On a plane.hs
+++ Codeforces/CF409G On a plane.hs
@@ -3,4 +3,4 @@ main = print . solve . map read . tail . words =<< getContents
 
 solve :: [Double] -> Double
 solve a = let b = map fst $ filter (odd . snd)  $ zip a [0..]
-          in  5.0 + sum b / (fromIntegral (length b))
+          in  5.0 + sum b / fromIntegral (length b)
